4d51bf89cf812de65ba610f258d330daf05b0d2b
Boyan <lbyybl1996@gmail.com>
2021-02-07 11:04:35 -0500

    fix navbar issue

--- arch-studio-multi-page-website/src/components/NavLinks.js
+++ arch-studio-multi-page-website/src/components/NavLinks.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavLinks = () => {
     const navigationlistLinks = ['portfolio', 'about', 'contact'];
     const renderList = navigationlistLinks.map((link, i) => (
         <li className="link-item" key={i}>
-            <Link to={`/${link}`}>{i === 1 ? link + ` Us` : link}</Link>
+            <NavLink activeClassName="hoverColor" to={`/${link}`}>
+                {i === 1 ? link + ` Us` : link}
+            </NavLink>
         </li>
     ));
 

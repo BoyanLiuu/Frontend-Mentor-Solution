d634c90544d7b589555277cd017c435705ce0ab2
Boyan <lbyybl1996@gmail.com>
2021-04-05 16:44:02 -0400

    testing code

--- social-media-dashboard-with-theme-switcher/src/App.js
+++ social-media-dashboard-with-theme-switcher/src/App.js
@@ -1,6 +1,9 @@
 import HomePage from './components/pages/HomePage';
 
 function App() {
+    let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    console.log("In the app");
+    console.log(isMobile);
     return <HomePage />;
 }
 

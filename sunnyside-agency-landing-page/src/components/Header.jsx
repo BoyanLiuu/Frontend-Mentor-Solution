8be73ac43509d40d6ecb529c6cbf0062a9e3ebbd
Boyanliuu <boyanliuu@gmail.com>
2023-09-30 17:21:29 -0700

    Wrap up sunny side agency landing page

--- sunnyside-agency-landing-page/src/components/Header.jsx
+++ sunnyside-agency-landing-page/src/components/Header.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Logo from './Logo';
 import Image from 'next/image';
 import { useWindowSize } from '../hooks/useWindowSize';
